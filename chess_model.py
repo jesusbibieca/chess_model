0100412594971450d8c7f98fa711d532b5b84100
Jesus Bibieca <jesusbibieca52@gmail.com>
2017-12-04 18:40:10 -0400

    add initial files and structs

---
+++ chess_model.py
@@ -0,0 +1,22 @@
+'''    Hola!
+	He aqui tu proyecto:
+	Desarrollar la logica de dos ficha (Reina y Peon) del juego de ajedrez
+	Se me olvido aclararte lo sgte:
+	Investiga sobre en que consiste el juego, te vas a basar lo aprendido en el 
+	curso programacion enfocado a objeto, no tienes que enfocarte en grafico ni 
+	nada parecido solo a nivel de logica .
+	
+	Cualquier pregunta puedes contactar a Ismael 
+	Tienes 1 mes para entregar el proyecto 
+
+    Suerte!
+'''
+
+class Piece(object):
+	pass
+
+class Pawn(Piece):
+	pass
+
+class Queen(Piece):
+	pass
